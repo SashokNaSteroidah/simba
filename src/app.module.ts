3be0edde6c2f35929aa7d925a7fb26fe36b264d6
merch2 <merch2@bwg-io.site>
2024-03-13 16:33:21 +0300

    dev: delete tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module}         from '@nestjs/common';
+import {AppController}  from './app.controller';
+import {AppService}     from './app.service';
+import {AuthModule}     from './auth/auth.module';
+import {DatabaseModule} from './database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports    : [AuthModule, DatabaseModule],
+    controllers: [AppController],
+    providers  : [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
