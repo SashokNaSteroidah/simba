d465226974ce451af85cf252748f734b52612b1f
merch2 <merch2@bwg-io.site>
2024-03-19 19:58:38 +0300

    ref: add decarator for roles

--- src/guards/roles/roles.guard.ts
+++ src/guards/roles/roles.guard.ts
@@ -1,24 +1,35 @@
 import {
     CanActivate,
     ExecutionContext,
+    HttpException,
+    HttpStatus,
     Injectable
 } from "@nestjs/common";
 import {jwtConstants} from "../constants/constants";
 import {JwtService} from "@nestjs/jwt";
-import {Roles} from "@prisma/client";
+import {Reflector} from "@nestjs/core";
+import {RolesGuardDecor} from "../../decorators/roles.decorator";
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-    constructor(private readonly jwtService: JwtService) {}
+    constructor(
+        private readonly jwtService: JwtService,
+        private reflector: Reflector
+    ) {}
     async canActivate(context: ExecutionContext): Promise<boolean> {
+        const roles = this.reflector.get(RolesGuardDecor, context.getHandler());
         const req   = context.switchToHttp().getRequest()
         const token = req.headers.cookie;
-        const payload = req['user'] = await this.jwtService.verifyAsync(
-            token,
-            {
-                secret: jwtConstants.secret
-            }
-        );
-        return payload.role === Roles.admin;
+        try {
+            const payload = req['user'] = await this.jwtService.verifyAsync(
+                token,
+                {
+                    secret: jwtConstants.secret
+                }
+            );
+            return payload.role === roles;
+        } catch (e) {
+            throw new HttpException("You don't have rights for this action", HttpStatus.FORBIDDEN);
+        }
     }
 }
