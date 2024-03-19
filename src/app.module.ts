e5c7f5b18b8971eecc11ad865e787e5a7de955b3
merch2 <merch2@bwg-io.site>
2024-03-19 19:59:08 +0300

    ref: add env support to module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import {AppController}  from './app.controller';
 import {AppService}     from './app.service';
 import {AuthModule}     from './auth/auth.module';
 import {DatabaseModule} from './database/database.module';
-import { PostModule } from './post/post.module';
+import {PostModule}     from './post/post.module';
+import {ConfigModule}   from "@nestjs/config";
 
 @Module({
-    imports    : [AuthModule, DatabaseModule, PostModule],
+    imports    : [
+            AuthModule,
+            DatabaseModule,
+            PostModule,
+            ConfigModule.forRoot({
+                isGlobal: true,
+            })
+        ],
     controllers: [AppController],
     providers  : [AppService],
 })
