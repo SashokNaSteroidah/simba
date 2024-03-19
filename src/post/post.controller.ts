33176477283d3558cf018268dcacce107cb341cd
merch2 <merch2@bwg-io.site>
2024-03-19 19:58:51 +0300

    ref: add role deco for posts

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -11,6 +11,10 @@ import {PostService}   from "./post.service";
 import {CreatePostDto} from "./types/createPost.dto";
 import {GetPostDto} from "./types/getPost.dto";
 import {AuthGuard}  from "../guards/auth/auth.guard";
+import {
+    RolesGuardDecor
+}                   from "../decorators/roles.decorator";
+import {Roles}         from "@prisma/client";
 import {RolesGuard}    from "../guards/roles/roles.guard";
 
 @Controller('post')
@@ -18,6 +22,7 @@ export class PostController {
     constructor(private readonly postService: PostService) {
     }
 
+    @RolesGuardDecor(Roles.admin)
     @UseGuards(AuthGuard)
     @UseGuards(RolesGuard)
     @UsePipes(new ValidationPipe())
